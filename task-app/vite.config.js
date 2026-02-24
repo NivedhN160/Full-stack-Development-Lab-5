ebc0262b66f4eca07e56f1cb25fd84ec754f8c45
NIVEDH SUNIL <nishikradha@gmail.com>
2026-02-24 11:52:02 +0530

    Fix deployment paths

--- task-app/vite.config.js
+++ task-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Full-stack-Development-Lab-5/task-app/',
+  base: '/Full-stack-Development-Lab-5/',
 })

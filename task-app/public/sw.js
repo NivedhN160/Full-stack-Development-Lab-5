6f813972ef8cf2e782a33511eccc95e29b6e32f9
NIVEDH SUNIL <nishikradha@gmail.com>
2026-02-25 19:46:56 +0530

    file

--- task-app/public/sw.js
+++ task-app/public/sw.js
@@ -2,14 +2,16 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("task-cache-v1").then((cache) => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
       ]);
     })
   );
 });
- 
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
--- task-app/src/main.jsx
+++ task-app/src/main.jsx
@@ -9,6 +9,6 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
+  navigator.serviceWorker.register("./sw.js");
 }
 
